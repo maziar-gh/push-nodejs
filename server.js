b66a5051ac434e7166d46bbc7ff1f56e57e13f83
maziar <maziar.gh.00@gmail,com>
2023-03-13 11:33:40 +0330

    [fix] all

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 // (A) SETTINGS - CHANGE TO YOUR OWN!
 const port = 80,
       mail = "your@email.com",
-      publicKey = "YOUR-PUBLIC-KEY",
-      privateKey = "YOUR-PRIVATE-KEY";
+      publicKey = "BJztwj9VH5ri7YbfJDTxz2qR3zObHc_rWq3ktzXo2ugU_NFTrhBG6yFxW3tUcYdj24-Mci_dVU1mEdX874aKlTI",
+      privateKey = "fIrHkH8BbL4ohQCdkxEqrAi6saPsGPipZyxXZCHw6sY";
  
 // (B) LOAD MODULES
 const express = require("express"),
@@ -17,7 +17,7 @@ app.use(express.static(__dirname)); // serve static files
 app.use(bodyParser.json()); // json parser
 
 // (D) SERVE TEST HOME PAGE
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/3-perm-sw.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/index.html")));
  
 // (E) SEND TEST PUSH NOTIFICATION
 app.post("/mypush", (req, res) => {
@@ -25,8 +25,8 @@ app.post("/mypush", (req, res) => {
   webpush.sendNotification(req.body, JSON.stringify({
     title: "Welcome!",
     body: "Yes, it works!",
-    icon: "i-loud.png",
-    image: "i-zap.png"
+    icon: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+    image: "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg"
   }))
   .catch(err => console.log(err));
 });
---
+++ vapid-keys.js
@@ -0,0 +1,7 @@
+const vapidKeys = require("web-push").generateVAPIDKeys();
+console.log(vapidKeys);
+
+var keys = {
+    publicKey: 'BJztwj9VH5ri7YbfJDTxz2qR3zObHc_rWq3ktzXo2ugU_NFTrhBG6yFxW3tUcYdj24-Mci_dVU1mEdX874aKlTI',
+    privateKey: 'fIrHkH8BbL4ohQCdkxEqrAi6saPsGPipZyxXZCHw6sY'
+  };
