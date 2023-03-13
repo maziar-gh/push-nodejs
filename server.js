6edf73312d1c8433057ce28d28aa8ec4d8480fed
maziar <maziar.gh.00@gmail,com>
2023-03-13 12:15:02 +0330

    [fix] change port

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // (A) SETTINGS - CHANGE TO YOUR OWN!
-const port = 3001,
+const port = 80,
       mail = "your@email.com",
       publicKey = "BJztwj9VH5ri7YbfJDTxz2qR3zObHc_rWq3ktzXo2ugU_NFTrhBG6yFxW3tUcYdj24-Mci_dVU1mEdX874aKlTI",
       privateKey = "fIrHkH8BbL4ohQCdkxEqrAi6saPsGPipZyxXZCHw6sY";
